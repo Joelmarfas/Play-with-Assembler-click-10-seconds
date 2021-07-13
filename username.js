d749a397d73cecec5767e2ac0ef484b32cd1272b
Josep Romero Ros <joseprrr@gmail.com>
2021-07-13 15:09:14 +0200

    Css basic

---
+++ username.js
@@ -0,0 +1,8 @@
+// Variable Declaration
+
+let startButton = document.getElementById("start_button");
+let userNameContainer = document.getElementsByClassName("username_container");
+let levelSelection = document.getElementsByClassName("level_selection");
+let userNameInput = document.getElementsByClassName("username_input");
+
+let regExpUserName = /^[A-Za-z0-9]+$/;
