85dff6f86d31f8a54cb61cea39e3a344bb586306
MarcelDurgante <marceldur@gmail.com>
2021-07-14 11:01:29 +0200

    last update

---
+++ roockie_level.js
@@ -0,0 +1,31 @@
+/* variables */
+/* let username = document.getElementById("username").innerHTML;
+
+
+
+console.log(username)
+
+
+
+let startBtn = document.getElementById('start_button');
+
+startBtn.addEventListener('click', () =>
+{
+    let input = document.getElementById('username').value;
+    alert(input); 
+}  */
+
+let count = 0;
+function myFunction() {
+  count+=1;
+    document.getElementById("btn").innerHTML = count;
+    
+     var clicks = 0;
+    function myFunction() {
+ 
+        clicks +=  1;
+        document.getElementById("demo").innerHTML = clicks;
+
+
+    }
+
