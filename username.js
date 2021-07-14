eb350e67b25850dc4f721d2495cca6edd5597792
Josep Romero Ros <joseprrr@gmail.com>
2021-07-14 10:34:12 +0200

    username screen done; select level screen done

--- username.js
+++ username.js
@@ -1,8 +1,40 @@
 // Variable Declaration
 
 let startButton = document.getElementById("start_button");
-let userNameContainer = document.getElementsByClassName("username_container");
-let levelSelection = document.getElementsByClassName("level_selection");
-let userNameInput = document.getElementsByClassName("username_input");
+let userNameContainer = document.getElementById("username_container");
+let levelSelection = document.getElementById("level_selection");
+let userName = document.getElementById("username");
+let alertBox = document.getElementById("alert_box");
+let validUsername;
 
 let regExpUserName = /^[A-Za-z0-9]+$/;
+
+// EVENT LISTENERS
+
+userName.addEventListener("input", invalidUsername);
+startButton.addEventListener("click", continues);
+
+// FUNCTIONS
+
+function invalidUsername(e){
+    
+    if(userName.value === "" || userName.value === null){
+        alertBox.innerHTML="Please introduce an UserName"
+    }
+    else if(!e.target.validity.valid ){
+        alertBox.innerHTML="Please use only letters or numbers"
+    }
+    else{
+        alertBox.innerHTML=null
+        validUserName = true;
+    }
+}
+
+function continues(e){
+    
+    if(validUserName===true){
+        userNameContainer.classList.add("is_hidden");
+        levelSelection.classList.remove("is_hidden");
+    }
+
+}
