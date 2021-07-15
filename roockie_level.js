4f78ff634ea7f8be1c262f6d59736de02a005090
MarcelDurgante <marceldur@gmail.com>
2021-07-15 06:23:45 +0200

    just organized JS structure for readability

--- roockie_level.js
+++ roockie_level.js
@@ -1,6 +1,31 @@
-/* FUNCTION THAT COUNTS THE CLICKS ON PRESSING CLICK ME BUTTON */
+//  VARIABLES 
 
 let clickMain = document.getElementById('click_roockie');
+let gameOver = document.getElementById('gameOver');
+let startGame = document.getElementById('start_G');
+let yourScore = document.getElementById('span_clicks');
+
+// FUNCTION THAT STARTS THE GAME
+
+startGame.addEventListener('click', hideG);
+
+function hideG() {
+	startGame.classList.add('is_hidden');
+	clickMain.classList.remove('is_hidden');
+	span_clicks.classList.add('is_hidden');
+	setTimeout(function () {
+		alert('Your time is up!!!!!');
+		if (alert) {
+			gameOver.classList.remove('is_hidden');
+			yourScore.classList.remove('is_hidden');
+			clickMain.classList.add('is_hidden');
+			yourScore = clicks;
+		}
+	}, 10000);
+}
+
+// FUNCTION THAT COUNTS THE CLICKS ON PRESSING HIT ME HARD BUTTON
+
 clickMain.addEventListener('click', countClicks);
 var counted = [];
 var clicks = 0;
@@ -8,27 +33,3 @@ function countClicks() {
 	clicks += 1;
 	counted = document.getElementById('clicks').innerHTML = clicks;
 }
-console.log(counted);
-
-// PRESSING START GAME MAKES ITSELF HIDE AND 'CLICK_ROOKIE' BUTTON APPEAR
-let gameOver = document.getElementById('gameOver')
-let startGame = document.getElementById('start_G');
-let yourScore = document.getElementById('span_clicks')
-
-startGame.addEventListener('click', hideG);
-
-function hideG() {
-	startGame.classList.add('is_hidden');
-  clickMain.classList.remove('is_hidden');
-  span_clicks.classList.add('is_hidden')
-		setTimeout(function () {
-      alert('Your time is up!!!!!')
-      if (alert) {
-        gameOver.classList.remove('is_hidden')
-        yourScore.classList.remove('is_hidden')
-        clickMain.classList.add('is_hidden');
-        yourScore = clicks
-      }
-    }, 3000);  
-	}
-
