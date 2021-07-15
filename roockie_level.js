c210d307e3ea8fc308a079f293b127bff98586cb
MarcelDurgante <marceldur@gmail.com>
2021-07-15 15:32:28 +0200

    stuff to check on merge

--- roockie_level.js
+++ roockie_level.js
@@ -1,19 +1,34 @@
-let clickMain = document.getElementById('click_rookie');
-clickMain.addEventListener('click', countClicks);
+//  VARIABLES
+
+
+
+// FUNCTION THAT STARTS THE GAME
+
+roockieStart.addEventListener('click', hideG);
+
+function hideG() {
+	roockieStart.classList.add('is_hidden');
+	clickRoockie.classList.remove('is_hidden');
+
+	setTimeout(function () {
+		counter = clicks;
+		ranking.push({ Username: userName, Score: counter });
+		finalScoreCont.classList.remove('is_hidden');
+		clickRoockie.classList.add('is_hidden');
+	}, 3000);
+}
+
+// FUNCTION THAT COUNTS THE CLICKS ON PRESSING HIT ME HARD BUTTON
+
+clickRoockie.addEventListener('click', countClicks);
 
 var clicks = 0;
 function countClicks() {
 	clicks += 1;
-	document.getElementById('clicks').innerHTML = clicks;
-}
-
-// PRESSING START GAME MAKES ITSELF HIDE AND 'CLICK_ROOKIE' BUTTON APPEAR
-let startGame = document.getElementById('start_G');
-startGame.addEventListener('click', hideG);
-function hideG() {
-	startGame.classList.add('is_hidden');
-	clickMain.classList.remove('is_hidden');
-	setTimeout(function () {
-		alert('Your time is up!!!!!');
-	}, 10000);
+	document.onkeydown = function (e) {
+		if (e.target === 32) {
+			//  USED 'Target' BECAUSE 'KeyCode' IS DEPRECATED
+			clickRoockie.click();
+		}
+	};
 }
--- selectLevelJS.js
+++ selectLevelJS.js
@@ -1,49 +1,42 @@
 // Variable Declaration
 
-let roockieBtn = document.getElementById("roockie");
-let hardBtn = document.getElementById("hard");
-let insaneBtn = document.getElementById("insane");
+let roockieBtn = document.getElementById('roockie');
+let hardBtn = document.getElementById('hard');
+let insaneBtn = document.getElementById('insane');
 
-let insaneStart=document.getElementById("start_insane");
-let insaneClick = document.getElementById("click_insane");
+let clickRoockie = document.getElementById('click_roockie');
+let roockieStart = document.getElementById('start_G');
 
-let roockieLvl= document.getElementById("roockie_level");
-let hardLvl= document.getElementById("hard_level");
-let insaneLvl= document.getElementById("insane_level");
+let insaneStart = document.getElementById('start_insane');
+let insaneClick = document.getElementById('click_insane');
 
-
+let roockieLvl = document.getElementById('roockie_level');
+let hardLvl = document.getElementById('hard_level');
+let insaneLvl = document.getElementById('insane_level');
 
 // EVENT LISTENERS
 
-
-roockieBtn.addEventListener("click", roockie);
-hardBtn.addEventListener("click", hard);
-insaneBtn.addEventListener("click", insane);
-
-
+roockieBtn.addEventListener('click', roockie);
+hardBtn.addEventListener('click', hard);
+insaneBtn.addEventListener('click', insane);
 
 // FUNCTIONS
 
-function roockie(){
-
-    levelSelection.classList.add("is_hidden");
-    roockieLvl.classList.remove("is_hidden");
-    startGame.classList.remove('is_hidden');
-	clickMain.classList.add('is_hidden');
-
+function roockie() {
+	levelSelection.classList.add('is_hidden');
+	roockieLvl.classList.remove('is_hidden');
+	roockieStart.classList.remove('is_hidden');
+	clickRoockie.classList.add('is_hidden');
 }
 
-function hard(){
-
-    levelSelection.classList.add("is_hidden");
-    hardLvl.classList.remove("is_hidden");
-
+function hard() {
+	levelSelection.classList.add('is_hidden');
+	hardLvl.classList.remove('is_hidden');
 }
 
-function insane(){
-
-    levelSelection.classList.add("is_hidden");
-    insaneLvl.classList.remove("is_hidden");
-    insaneStart.classList.remove("is_hidden");
-    insaneClick.classList.add("is_hidden");
+function insane() {
+	levelSelection.classList.add('is_hidden');
+	insaneLvl.classList.remove('is_hidden');
+	insaneStart.classList.remove('is_hidden');
+	insaneClick.classList.add('is_hidden');
 }
